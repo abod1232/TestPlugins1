e43dcedafd25ab58e0acb6dd74cc70f45649b7c3
abod1232 <abgost4@outlook.com>
2025-11-13 20:07:43 +0300

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,36 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+// In the build.gradle.kts INSIDE your Arabseed folder
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
-// Use an integer for version numbers
-version = 1
-
-cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
+    namespace = "com.arabseed"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-XXLanguage:+BreakContinueInInlineLambdas")
+    }
+}
+
+dependencies {
+    // FIXME remove this when crossplatform is fully supported
+    val cloudstream by configurations
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.browser:browser:1.9.0")
+    implementation("androidx.room:room-ktx:2.8.0")
+    cloudstream("com.lagradost:cloudstream3:pre-release")
 }
